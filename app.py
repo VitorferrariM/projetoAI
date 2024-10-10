7096f071bc03a5bcfe3b4f1e29b58f5acf3039bc
Seu Nome <seuemail@example.com>
2024-10-10 13:07:52 -0300

    nova tentativa

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from pydantic import ValidationError
 from database import salvar_no_postgres
 
 def main():
-    st.title("Sistema de Vendas")
-    
+    st.title("Sistema de CRM e Vendas da ZapFlow - Frontend Simples")
     email = st.text_input("Campo de texto para inserção do email do vendedor")
     data = st.date_input("Data da compra", datetime.now())
     hora = st.time_input("Hora da compra", value=time(9, 0))  # Valor padrão: 09:00
@@ -17,20 +16,19 @@ def main():
     if st.button("Salvar"):
         try:
             data_hora = datetime.combine(data, hora)
-       
             
             venda = Vendas(
-                email=email,
-                data=data_hora,
-                valor=valor,
-                quantidade=quantidade,
-                produto=produto
+                email = email,
+                data = data_hora,
+                valor = valor,
+                quantidade = quantidade,
+                produto = produto
             )
-            
             st.write(venda)
             salvar_no_postgres(venda)
         except ValidationError as e:
-            st.error(f"Erro na validação: {e}")
+            st.error(f"Deu erro {e}")
 
-if __name__ == "__main__":
+            
+if __name__=="__main__":
     main()
--- contrato.py
+++ contrato.py
@@ -1,16 +1,17 @@
 from datetime import datetime
+from typing import Tuple
 from pydantic import BaseModel, EmailStr, PositiveFloat, PositiveInt
 from enum import Enum
 
-# Enum para os produtos
 class ProdutoEnum(str, Enum):
     produto1 = "ZapFlow com Gemini"
     produto2 = "ZapFlow com chatGPT"
     produto3 = "ZapFlow com Llama3.0"
 
-# Classe de vendas
 class Vendas(BaseModel):
-    email: EmailStr
+    
+
+    email: EmailStr # kaio =! kaio@gmai.com
     data: datetime
     valor: PositiveFloat
     quantidade: PositiveInt
